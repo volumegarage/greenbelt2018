506c109c024d95480f762c0e9dc193af0526c35b
David Cate <davidcate@2600-6c5c-6c00-3952-b89c-9739-b271-7679.dhcp6.chtrptr.net>
2018-02-01 22:21:38 -0500

    LatLong Functions Implemented

--- 2018_Greenbelt/ViewController.swift
+++ 2018_Greenbelt/ViewController.swift
@@ -7,11 +7,19 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
     
     let greenbeltLogo = UIImageView()
     
+    //Constants
+    let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
+    let APP_ID = "3d08d5a0bbecd09a6d7dfe700c81e14f"
+    
+    let locationManager = CLLocationManager()
+    
+    @IBOutlet weak var cityLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,7 +37,19 @@ class ViewController: UIViewController {
         siteLaunch()
         
         
+        //TODO:Set up the location manager here.
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+
+        
     }
+    
+    
+    //MARK: - Site Launch Method Animations
+    /***************************************************************/
     
     @objc func siteLaunch() {
         
@@ -40,6 +60,44 @@ class ViewController: UIViewController {
         }
         animator.startAnimation(afterDelay: 1)
     }
+    
+    
+    
+    //MARK: - Location Manager Delegate Methods
+    /***************************************************************/
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[locations.count - 1]
+        if location.horizontalAccuracy > 0 {
+            locationManager.stopUpdatingLocation()
+            
+            print("longitude = \(location.coordinate.longitude), latitude = \(location.coordinate.latitude)")
+            
+            let latitude = String(location.coordinate.latitude)
+            let longitude = String(location.coordinate.longitude)
+            
+            let params : [String : String] = ["lat" : latitude, "lon" : longitude, "appid" : APP_ID]
+        }
+        
+    }
+    
+    //Write the didUpdateLocations method here:
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+        cityLabel.text = "Location Unavailable"
+    }
+    
+    
+    
+    //Write the didFailWithError method here:
+    
+    
+    
+    
+    
+    
+    
+    
     
 
     override func didReceiveMemoryWarning() {
