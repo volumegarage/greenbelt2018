eb1bd730434896665ce15f13aeb03a9d55dce920
David Cate <davidcate@2600-6c5c-6c00-3952-b89c-9739-b271-7679.dhcp6.chtrptr.net>
2018-02-01 21:06:26 -0500

    Added Animation for Logo on Home Page

--- 2018_Greenbelt/ViewController.swift
+++ 2018_Greenbelt/ViewController.swift
@@ -9,11 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let greenbeltLogo = UIImageView()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        greenbeltLogo.frame = CGRect(x: 120, y: -200, width: 190, height: 141)
+        greenbeltLogo.contentMode = .scaleAspectFill
+        greenbeltLogo.image = #imageLiteral(resourceName: "greenbeltlogo.png")
+        
+        view.addSubview(greenbeltLogo)
+        
+        self.view = view
+        
+        siteLaunch()
+        
+        
     }
+    
+    @objc func siteLaunch() {
+        
+        let animator = UIViewPropertyAnimator(duration: 1.5, dampingRatio: 0.7) {
+            
+            self.greenbeltLogo.frame = CGRect(x: 120, y: 140, width: 190, height: 141)
+            
+        }
+        animator.startAnimation(afterDelay: 1)
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
---
+++ 2018_Greenbelt/WeatherViewController.swift
@@ -0,0 +1,111 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Angela Yu on 23/08/2015.
+//  Copyright (c) 2015 London App Brewery. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+
+class WeatherViewController: UIViewController, CLLocationManagerDelegate {
+    
+    //Constants
+    let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
+    let APP_ID = "3d08d5a0bbecd09a6d7dfe700c81e14f"
+    
+    
+    //TODO: Declare instance variables here
+    
+    let locationManager = CLLocationManager()
+    
+    
+    
+    //Pre-linked IBOutlets
+    @IBOutlet weak var weatherIcon: UIImageView!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        //TODO:Set up the location manager here.
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestWhenInUseAuthorization()
+        
+        
+        
+    }
+    
+    
+    
+    //MARK: - Networking
+    /***************************************************************/
+    
+    //Write the getWeatherData method here:
+    
+    
+    
+    
+    
+    
+    
+    //MARK: - JSON Parsing
+    /***************************************************************/
+    
+    
+    //Write the updateWeatherData method here:
+    
+    
+    
+    
+    
+    //MARK: - UI Updates
+    /***************************************************************/
+    
+    
+    //Write the updateUIWithWeatherData method here:
+    
+    
+    
+    
+    
+    
+    //MARK: - Location Manager Delegate Methods
+    /***************************************************************/
+    
+    
+    //Write the didUpdateLocations method here:
+    
+    
+    
+    //Write the didFailWithError method here:
+    
+    
+    
+    
+    
+    //MARK: - Change City Delegate methods
+    /***************************************************************/
+    
+    
+    //Write the userEnteredANewCityName Delegate method here:
+    
+    
+    
+    //Write the PrepareForSegue Method here
+    
+    
+    
+    
+    
+}
+
+
+
