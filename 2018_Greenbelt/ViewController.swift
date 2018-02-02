142eee1c6923a8dd2bd4a6472f3d6c50424e0e38
David Cate <davidcate@davids-imac.local>
2018-02-01 20:03:59 -0500

    Initial Commit

---
+++ 2018_Greenbelt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  2018_Greenbelt
+//
+//  Created by David Cate on 2/1/18.
+//  Copyright © 2018 David Cate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ 2018_GreenbeltTests/_018_GreenbeltTests.swift
@@ -0,0 +1,36 @@
+//
+//  _018_GreenbeltTests.swift
+//  2018_GreenbeltTests
+//
+//  Created by David Cate on 2/1/18.
+//  Copyright © 2018 David Cate. All rights reserved.
+//
+
+import XCTest
+@testable import _018_Greenbelt
+
+class _018_GreenbeltTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
